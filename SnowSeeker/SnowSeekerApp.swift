9bda3bf12d5c4f1dffbc96433784d66a74e467d1
Maxim Vainikka <maxim.vainikka@yandex.ru>
2022-01-30 09:42:11 +0300

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Максим Нуждин on 30.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
